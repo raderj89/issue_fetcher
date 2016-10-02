e8b551938826b043d023d7428243e1917fe8d4ab
Jared Rader <raderj89@gmail.com>
2016-10-02 16:13:30 -0700

    print list of issues in a table

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -8,7 +8,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -38,5 +40,59 @@ defmodule Issues.CLI do
 
       _ -> :help
     end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [count | #{@default_count}]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response
+    |> convert_to_list_of_maps
+    |> sort_into_ascending_order
+    |> Enum.take(count)
+    |> to_table
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues,
+              fn i1, i2 -> i1["created_at"] <= i2["created_at"] end
+  end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    {_, message} = List.keyfind(error, "message", 0)
+    IO.puts "Error fetching from GitHub: #{message}"
+    System.halt(2)
+  end
+
+  def convert_to_list_of_maps(list) do
+    list
+    |> Enum.map(&Enum.into(&1, Map.new))
+  end
+
+  def to_table(list_of_maps) do
+    header    = " #   | created_at           | title                         "
+    separator = "-----+----------------------+-------------------------------"
+    IO.puts(header)
+    IO.puts(separator)
+    format_data(list_of_maps)
+  end
+
+  def format_data([]), do: "done"
+
+  def format_data([head | tail]) do
+    format_row(head)
+    |> IO.puts
+    format_data(tail)
+  end
+
+  def format_row(row) do
+    "#{row["number"]} | #{row["created_at"]} | #{row["title"]}"
   end
 end
