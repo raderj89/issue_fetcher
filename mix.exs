4fe36f6fc10ce1038c36a8e983f8a9a4813750c6
Jared Rader <raderj89@gmail.com>
2016-10-03 12:10:30 -0700

    test docs; add GitHub info

--- mix.exs
+++ mix.exs
@@ -3,6 +3,8 @@ defmodule Issues.Mixfile do
 
   def project do
     [app: :issues,
+     name: "GitHub Issues Fetcher",
+     source_url: "https://github.com/raderj89/issue_fetcher"
      escript: escript_config,
      version: "0.1.0",
      elixir: "~> 1.3",
@@ -30,7 +32,9 @@ defmodule Issues.Mixfile do
   defp deps do
     [
       httpoison: "~> 0.8",
-      poison: "~> 1.5"
+      poison: "~> 1.5",
+      ex_doc: "~> 0.11",
+      earmark: ">= 0.0.0"
     ]
   end
 
